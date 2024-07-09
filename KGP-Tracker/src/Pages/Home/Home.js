96c99c10865f00047bc994abdd133e875e119526
coffeetunes <hello@coffeetunes.pl>
2024-07-09 22:47:32 +0200

    dodanie slajdera na stronę główną

--- KGP-Tracker/src/Pages/Home/Home.js
+++ KGP-Tracker/src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import "./Home.scss"
 import { useAuth } from '../../context/AuthContext';
 import Button from 'react-bootstrap/Button';
+import {Carousel} from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -12,7 +13,53 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="container mt-5">
+<>
+        <Carousel className="main-carousel">
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={require("../../assets/images/slide-1.jpg")}
+                    alt="First slide"
+                />
+                <div className="overlay"></div>
+                <Carousel.Caption>
+                    <h3>Główne Hasło 1</h3>
+                    <p>Podhasło 1</p>
+                    <Button variant="primary">Przycisk 1</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={require("../../assets/images/slide-2.jpg")}
+                    alt="Second slide"
+                />
+                <div className="overlay"></div>
+                <Carousel.Caption>
+                    <h3>Główne Hasło 2</h3>
+                    <p>Podhasło 2</p>
+                    <Button variant="primary">Przycisk 2</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={require("../../assets/images/slide-3.jpg")}
+                    alt="Third slide"
+                />
+                <div className="overlay"></div>
+                <Carousel.Caption>
+                    <h3>Główne Hasło 3</h3>
+                    <p>Podhasło 3</p>
+                    <Button variant="primary">Przycisk 3</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+       {/* <div className="container mt-5">
+
+
+
             <h1 className="main-heading">Strona Główna</h1>
 
             {isLoggedIn ? (
@@ -26,7 +73,8 @@ const Home = () => {
                     <Button as={Link} to="/login" variant="primary">Zaloguj się</Button>
                 </div>
             )}
-        </div>
+        </div>*/}
+</>
     );
 };
 
