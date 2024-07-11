0f495c193881868691956afa2a97301f50d3dd49
coffeetunes <hello@coffeetunes.pl>
2024-07-11 22:20:06 +0200

    dodanie kolejnego slajdu oraz logiki widoczności slajdów w zależności od zalogowania

--- KGP-Tracker/src/Pages/Home/Home.js
+++ KGP-Tracker/src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import "./Home.scss"
 import { useAuth } from '../../context/AuthContext';
 import Button from 'react-bootstrap/Button';
-import {Carousel} from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -13,51 +13,68 @@ const Home = () => {
     }, []);
 
     return (
-<>
+        <>
 
-        <Carousel className="main-carousel">
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../../assets/images/slide-1.jpg")}
-                    alt="First slide"
-                />
-                <div className="overlay"></div>
-                <Carousel.Caption>
-                    <h3>Twój osobisty dziennik wypraw KGP</h3>
-                    <p>Monitoruj swoje zdobycze</p>
-                    <Button variant="primary" as={Link} to="/login" className="carousel-button">Zaloguj się</Button>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../../assets/images/slide-2.jpg")}
-                    alt="Second slide"
-                />
-                <div className="overlay"></div>
-                <Carousel.Caption>
-                    <h3>Odkrywaj Polskie Góry</h3>
-                    <p>Znajdź i zdobywaj szczyty Korony Gór Polski</p>
-                    <Button variant="primary" as={Link} to="/peaks" className="carousel-button">Sprawdź szczyty</Button>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../../assets/images/slide-3.jpg")}
-                    alt="Third slide"
-                />
-                <div className="overlay"></div>
-                <Carousel.Caption>
-                    <h3>Zdobądź z nami Koronę Gór Polski</h3>
-                    <p>Utwórz konto i zacznij swoją przygodę z górami!</p>
-                    <Button variant="primary" as={Link} to="/register" className="carousel-button">Zarejestruj się</Button>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+            <Carousel className="main-carousel">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={require("../../assets/images/slide-2.jpg")}
+                        alt="Second slide"
+                    />
+                    <div className="overlay"></div>
+                    <Carousel.Caption>
+                        <h3>Odkrywaj Polskie Góry</h3>
+                        <p>Znajdź i zdobywaj szczyty Korony Gór Polski</p>
+                        <Button variant="primary" as={Link} to="/peaks" className="carousel-button green-button">Sprawdź szczyty</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                {!isLoggedIn &&
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={require("../../assets/images/slide-1.jpg")}
+                            alt="First slide"
+                        />
+                        <div className="overlay"></div>
+                        <Carousel.Caption>
+                            <h3>Twój osobisty dziennik wypraw KGP</h3>
+                            <p>Monitoruj swoje zdobycze</p>
+                            <Button variant="primary" as={Link} to="/login" className="carousel-button green-button">Zaloguj się</Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>}
+                {!isLoggedIn && <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={require("../../assets/images/slide-3.jpg")}
+                        alt="Third slide"
+                    />
+                    <div className="overlay"></div>
+                    <Carousel.Caption>
+                        <h3>Zdobądź z nami Koronę Gór Polski</h3>
+                        <p>Utwórz konto i zacznij swoją przygodę z górami!</p>
+                        <Button variant="primary" as={Link} to="/register" className="carousel-button green-button">Zarejestruj się</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>}
 
-</>
+                {isLoggedIn &&
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={require("../../assets/images/slide-4.jpg")}
+                            alt="Third slide"
+                        />
+                        <div className="overlay"></div>
+                        <Carousel.Caption>
+                            <h3>{user.name}, ile zostało do zdobycia Korony?</h3>
+                            <p>Wejdź na swój profil i sprawdź postęp!</p>
+                            <Button variant="primary" as={Link} to="/profile" className="carousel-button green-button">Przejdź do profilu</Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                }
+            </Carousel>
+
+        </>
     );
 };
 
